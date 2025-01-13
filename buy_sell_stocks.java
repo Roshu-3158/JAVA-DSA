a63e0d95226ac2325cc58cbe67b3a35d8a6a18c2
Roshu-3158 <roshandnikam3158@gmail.com>
2025-01-13 23:08:47 +0530

    best time to buy and sell stocks

---
+++ buy_sell_stocks.java
@@ -0,0 +1,26 @@
+public class buy_sell_stocks {
+    public static int best_time(int prices[]){
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for(int i=0; i<prices.length; i++){
+            if(buyprice < prices[i]){
+                int profit = prices[i]-buyprice;
+                maxprofit =  Math.max(maxprofit, profit);
+            }else{
+                buyprice = prices[i];
+            }
+            
+        }
+        return maxprofit;
+    }
+
+
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+
+        System.out.println("Max Profit we can earn is : " +best_time(prices));
+        
+    }
+    
+}
