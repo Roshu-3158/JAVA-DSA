147526facbd2202659177e2d0f2896f781191fb4
Roshan Dattu Nikam <120217863+Roshu-3158@users.noreply.github.com>
2025-01-18 00:00:36 +0530

    diagonal sum

--- spiral_matrix.java
+++ spiral_matrix.java
@@ -42,12 +42,52 @@ public class spiral_matrix {
     } 
 
 
+    public static int digonalsum(int matrix[][]){
+        int sum = 0;
+
+        // with time complexity of O(N^2)
+        // for(int i=0; i<matrix.length;  i++){
+        //     for(int j=0; j<matrix[0].length; j++){
+        //         // primary diagonal i==j
+        //         if(i==j){
+        //             sum += matrix[i][j];
+        //         }
+
+        //         // secondary diagonal i+j = n-1
+        //         if(i+j == matrix.length-1){
+        //             sum += matrix[i][j];
+        //         }
+
+        //     }
+        // }
+
+        for(int i=0; i<matrix.length; i++){
+            // primary diagonal 
+            sum += matrix[i][i];
+
+            // secondary diagonal 
+            if(i != matrix.length-1-i){
+                sum+= matrix[i][matrix.length-i-1];
+            }
+
+            
+        }
+
+
+
+
+        return sum;
+    }
+
+
     public static void main(String[] args) {
         int matrix[][] = {{1,2,3,4},
                           {5,6,7,8},
                           {9,10,11,12},
                           {13,14,15,16}};
-        printspiral(matrix);
+        // printspiral(matrix);
+
+        System.out.println(digonalsum(matrix));
     }
 }
 
