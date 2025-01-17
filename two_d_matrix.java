86e03adc14d05489726c53079de168184a4b3260
Roshan Dattu Nikam <120217863+Roshu-3158@users.noreply.github.com>
2025-01-17 23:46:43 +0530

    Add files via upload
    2D Array

---
+++ two_d_matrix.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class two_d_matrix {
+
+
+    public static void search(int matrix[][], int key, int row, int col){
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(matrix[i][j] == key){
+                    System.out.println("Element found at "+i+" "+j);
+                }
+            }
+
+        }
+    }
+    public static void main(String[] args) {
+        // create 2d matrix 
+        int row=3;
+        int col=3;
+        int[][] matrix = new int[row][col];
+
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(" ");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                System.out.print(" "+matrix[i][j]);
+            }
+            System.out.println();
+        }  
+
+        search(matrix, 3, row, col);
+    }
+}
