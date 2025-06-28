b154c853e0142c7f30deb78dfd7c95fa569b42c8
Roshu-3158 <roshandnikam3158@gmail.com>
2025-06-28 22:51:42 +0530

    search rotated array

--- 20_Divide_and_conqure/search_rotated_array.java
+++ 20_Divide_and_conqure/search_rotated_array.java
@@ -2,10 +2,52 @@
 
 public class search_rotated_array {
 
-     
-    public static void main(String[] args) {
-        int [] arr = {4, 5, 6, 7, 0, 1, 2};
-        int target = 0; 
+    public static int search(int arr[], int tar, int si, int ei) {
+        // base case : does not exist
+        if (si > ei) {
+            return -1;
+        }
+
+        // mid index
+        int mid = si + (ei - si) / 2;
+
+        if (arr[mid] == tar) {
+            return mid;
+        }
+ 
+        // mid on line 1
+        if (arr[si] <= arr[mid]) {
+            // case a : left
+            if (arr[si] <= tar && tar <= arr[mid]) {
+                return search(arr, tar, si, mid);
+            }
+            // case a : right
+            else {
+                return search(arr, tar, mid + 1, ei);
+            }
+        }
+        // mid on line 2
+        else {
+            // case c : left
+            if (arr[mid] >= tar && tar >= arr[ei]) {
+                return search(arr, tar, si, mid - 1);
+            }
+            // case d : right
+            else {
+                return search(arr, tar, mid + 1, ei);
+            }
+
+        }
+
     }
-    
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 6; // output index -> 4
+        int tarIDX = search(arr, target, 0, arr.length);
+
+        System.out.println("Ans is : " + tarIDX);
+
+    }
+
 }
