0171f576e2f38565a754e9c698732504d7e95431
Roshu-3158 <roshandnikam3158@gmail.com>
2025-07-07 16:47:46 +0530

    Queue

---
+++ 27_Queue/Deque_creation.java
@@ -0,0 +1,16 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Deque_creation {
+
+    public static void main(String[] args) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+
+        System.out.println(deque);
+        deque.removeFirst();
+        System.out.println(deque);
+        
+    }
+}
